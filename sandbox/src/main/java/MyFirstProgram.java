3c61f086213e5fc30048bad039cf5ec83960bbbb
NataliaVolkova <ganesha_sun@mail.ru>
2021-03-03 13:09:43 +0300

    Задание №2: Потренироваться использовать функции, классы, объекты и методы

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -2,5 +2,23 @@ public class MyFirstProgram{
 
     public static void main(String[] args){
         System.out.println ("Hello, world!");
+
+        //Задание 2.
+        Point p1 = new Point(12,10);
+        Point p2 = new Point(15,-20);
+        System.out.println("Run separate function " + distance(p1,p2));
+
+        //Вычисление расстояния между точками при помощи метода в классе Point
+        double result = p1.distancePointMethod(p1,p2);
+        System.out.println("Run method from class Point " + result);
+
     }
+
+
+    //Функция вычисления расстояния между точками
+    public static double distance (Point p1, Point p2){
+        return Math.sqrt(Math.pow((p2.x - p1.x),2) + Math.pow((p2.y - p1.y),2));
+    }
+
+
 }
---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,15 @@
+public class Point {
+  double x;
+  double y;
+
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double distancePointMethod (Point p1, Point p2){
+    return Math.sqrt(Math.pow((p2.x - p1.x),2) + Math.pow((p2.y - p1.y),2));
+  }
+
+}
