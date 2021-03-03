7fea9bbab337d5694b8e42cd8e69ccf41ca00688
NataliaVolkova <ganesha_sun@mail.ru>
2021-03-03 17:55:17 +0300

    Задание №3: Подключение тестового фреймворка и  первые тесты

---
+++ sandbox/src/test/java/PointDistanceTests.java
@@ -0,0 +1,20 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointDistanceTests {
+
+  @Test
+  public void testDistance(){
+    Point p1 = new Point(12,10);
+    Point p2 = new Point(15,-20);
+    Assert.assertEquals(p1.distancePointMethod(p1,p2),30.14962686336267 );
+  }
+
+  @Test
+  public void testDistanceNegative(){
+    Point p1 = new Point(0,1);
+    Point p2 = new Point(2,3);
+    Assert.assertNotEquals(p2.distancePointMethod(p1,p2),0);
+
+  }
+}
