e4f3cc763fde18b94d93a9464666cf65383434d4
NataliaVolkova <ganesha_sun@mail.ru>
2021-03-06 14:26:18 +0300

    Задание №2: Потренироваться использовать функции, классы, объекты и методы

--- sandbox/src/test/java/PointDistanceTests.java
+++ sandbox/src/test/java/PointDistanceTests.java
@@ -7,14 +7,14 @@ public class PointDistanceTests {
   public void testDistance(){
     Point p1 = new Point(12,10);
     Point p2 = new Point(15,-20);
-    Assert.assertEquals(p1.distancePointMethod(p1,p2),30.14962686336267 );
+    Assert.assertEquals(p1.distancePointMethod(p2),30.14962686336267 );
   }
 
   @Test
   public void testDistanceNegative(){
     Point p1 = new Point(0,1);
     Point p2 = new Point(2,3);
-    Assert.assertNotEquals(p2.distancePointMethod(p1,p2),0);
+    Assert.assertNotEquals(p2.distancePointMethod(p2),0);
 
   }
 }
