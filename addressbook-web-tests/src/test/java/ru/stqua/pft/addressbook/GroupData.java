237754948c9921ecba8b8329a62a7dc5f5feda6c
Ganesha_sun <ganesha_sun@mail.ru>
2021-03-07 14:53:25 +0300

    Задание №4: Сделать тесты для создания групп и контактов в адресной книге

---
+++ addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/GroupData.java
@@ -0,0 +1,25 @@
+package ru.stqua.pft.addressbook;
+
+public class GroupData {
+  private final String name;
+  private final String header;
+  private final String footer;
+
+  public GroupData(String name, String header, String footer) {
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  public String name() {
+    return name;
+  }
+
+  public String header() {
+    return header;
+  }
+
+  public String footer() {
+    return footer;
+  }
+}
