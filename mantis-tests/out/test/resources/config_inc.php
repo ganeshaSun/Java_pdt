33ae7f2a10635539a7e5cd2a58698b9417e934cb
Ganesha_sun <ganesha_sun@mail.ru>
2021-04-26 16:55:30 +0300

    Лекция 8.7 Получение почты на внешний почтовый сервер

---
+++ mantis-tests/out/test/resources/config_inc.php
@@ -0,0 +1,16 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+$g_default_language = "english";
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = '5MOHC59//XxeUvugZAT5qrzYKKXBE4j3qcByQAjCr7c=';
+
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
+
+$g_signup_use_captcha = OFF;
